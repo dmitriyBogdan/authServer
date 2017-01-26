68fc148892d236521bf738a7433c379e230e0769
Ivan Lomakov <ivanlomakov@nixsolutions.com>
2017-01-26 13:16:52 +0200

    Drop down for register page is improved

--- IdentityServer/src/AuthServerDemo/Models/Client/ClientModel.cs
+++ IdentityServer/src/AuthServerDemo/Models/Client/ClientModel.cs
@@ -5,16 +5,21 @@ namespace AuthServerDemo.Models.Client
 {
     public class ClientModel
     {
+        [Display(Name = "Client Id")]
         public string ClientId { get; set; }
 
+        [Display(Name = "Client Name")]
         public string ClientName { get; set; }
 
         public string Secret { get; set; }
 
+        [Display(Name = "Redirect Uri")]
         public string RedirectUri { get; set; }
 
+        [Display(Name = "LogOut Redirect Uri")]
         public string LogOutRedirectUri { get; set; }
 
+        [Display(Name = "Grant Type")]
         public string GrantType { get; set; }
 
         public IEnumerable<string> AllowedScopes { get; set; }
